1f95424188b39540352460c1a25cd517ab32d217
meddizzle316 <meddizzle316@gmail.com>
2023-09-20 08:55:07 -0700

    why betty why

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - just making a method
+ *
+ * Return: literally just a zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
