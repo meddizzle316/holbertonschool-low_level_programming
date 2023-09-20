2f5dca5c59a6600643d254a9d7255ec5bbe4a2a9
meddizzle316 <meddizzle316@gmail.com>
2023-09-20 08:45:31 -0700

    freaking betty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
-
+/**
+ *main-Entry point
+ *
+ *Return:literally just printing a line
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
