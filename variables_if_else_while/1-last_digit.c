0946ab8556d2d04cdb046324b81cd0f34b347d03
meddizzle316 <meddizzle316@gmail.com>
2023-09-20 13:49:35 -0700

    git wrecked betty finally

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,13 +17,13 @@ int main(void)
 	last_d = n % 10;
 	if (last_d > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 4\n", n, last_d);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_d);
 	}
 	if (last_d == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, last_d);
 	}
-	if (last_d > 6 && last_d != 0)
+	if (last_d < 6 && last_d != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_d);
 	}
