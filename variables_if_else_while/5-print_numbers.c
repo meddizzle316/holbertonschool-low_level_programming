fadb42946233b20e1b8e17a1af81fba2e8e4961e
meddizzle316 <meddizzle316@gmail.com>
2023-09-21 11:47:36 -0700

    trying out my scripts

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - printing all single digit numbers 
+ *
+ * Return: all the single ladies
+ */
+int main(void)
+{
+	int i;
+
+	for ( ; i < 10; i++)
+	{
+		printf(i);
+	}
+	return (0);
+}
