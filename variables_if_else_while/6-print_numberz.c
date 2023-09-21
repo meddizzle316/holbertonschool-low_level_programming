cfd4cc69aa7fe0e76efb8a03503971a7a771263d
meddizzle316 <meddizzle316@gmail.com>
2023-09-21 12:07:25 -0700

    first try on 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - function
+ *
+ * Return: prints all single digits (base 10) starting from zero with new line
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	for ( ; i < 0; )
+	{
+		putchar('0' + i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
