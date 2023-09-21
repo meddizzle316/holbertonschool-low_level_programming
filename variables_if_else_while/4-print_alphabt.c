78629969a58e2707de566d92d89a8749ed8cd6b3
meddizzle316 <meddizzle316@gmail.com>
2023-09-21 10:47:37 -0700

    first attempt

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - printing alphabet in lowercase
+ *
+ * Return: ditto
+ */
+int main(void)
+{
+	char counter;
+
+	for (counter = 97; counter < 123; counter++)
+	{
+		if (counter == 113 || counter == 101)
+		{
+			break;
+		}
+		else
+		{
+			putchar(counter);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
