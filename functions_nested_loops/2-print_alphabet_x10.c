2f17fc85cd6c9cbeaba8a70542a0995f41990123
meddizzle316 <meddizzle316@gmail.com>
2023-09-22 10:00:45 -0700

    asouidhfglasndgloi

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -6,20 +6,20 @@
  */
 void print_alphabet_x10(void)
 {
-        int i;
+	int i;
 
-        int x;
+	int x;
 
-        i = 97;
-        x = 1;
-        for ( ; x < 5; x++)
-        {
-                x++;
-                for ( ; i < 123; )
-                {
-                        _putchar(i);
-                        i++;
-                }
-        }
-        _putchar('\n');
+	i = 97;
+	x = 1;
+	for ( ; x < 5; x++)
+	{
+		for ( ; i < 123; )
+		{
+			_putchar(i);
+			i++;
+		}
+		i = 97;
+	}
+	_putchar('\n');
 }
