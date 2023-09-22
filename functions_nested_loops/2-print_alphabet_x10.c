c2200b2d4c6cfc474a8c6010e04fe8dc543a5553
meddizzle316 <meddizzle316@gmail.com>
2023-09-22 09:55:02 -0700

    asouidhfglasndgloi

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -- do stuff
+ *
+ * Return: prints x10 the lowercase alphabet
+ */
+void print_alphabet_x10(void)
+{
+        int i;
+
+        int x;
+
+        i = 97;
+        x = 1;
+        for ( ; x < 5; x++)
+        {
+                x++;
+                for ( ; i < 123; )
+                {
+                        _putchar(i);
+                        i++;
+                }
+        }
+        _putchar('\n');
+}
