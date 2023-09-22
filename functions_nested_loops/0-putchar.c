e3cc50f4f28bc8e154a47570fbbbd5753826fb8c
meddizzle316 <meddizzle316@gmail.com>
2023-09-22 08:45:28 -0700

    git wrecked

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * putchar - prints with a new line
  *
  * Return: ditto
  */
-int _putchar(char x)
+int main(char c)
 {
-	putchar("_");
-	putchar("p");
-	putchar("u");
-	putchar("t");
-	putchar("c");
-	putchar("h");
-	putchar("a");
-	putchar("r");
-	putchar("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * File: main.h
+ * Auth: David
+ * Desc: Header file
+ */
+
 int _putchar(char c);
 
 #endif
