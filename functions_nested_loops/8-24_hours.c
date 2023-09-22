e41ab8bb6848554293dd6fe4335c655268fda66e
meddizzle316 <meddizzle316@gmail.com>
2023-09-22 12:14:35 -0700

    first attempt

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: giving the minute of every day
+ */
+void jack_bauer(void)
+{
+	int hour;
+
+	int min; 
+
+	hour = 0;
+	min = 0;
+	for ( ; hour < 24; hour++)
+	{
+		for ( ; min < 60; min++)
+		{
+			_putchar('0' + hour);
+			_putchar(':');
+			_putchar('0' + min);
+		}
+	}
+}	
