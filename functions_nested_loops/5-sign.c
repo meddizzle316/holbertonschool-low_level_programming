2402e3a85f013e0ba31d8d0e7d5b946e37afcb65
meddizzle316 <meddizzle316@gmail.com>
2023-09-22 10:46:13 -0700

    first attempt good feelings yes yes yes

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n - the given number
+ * Return: 0 if 0 and 1 if positive and -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n < 0)
+	{
+		return (-1);
+		_putchar('-');
+	}
+	else
+		return (0);
+		_putchar(0);
+}
