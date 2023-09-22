1e0d19f8c2eacf6d6da45fd6637e3763ec489c63
meddizzle316 <meddizzle316@gmail.com>
2023-09-22 13:45:53 -0700

    first try good feeling

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * times_table - printing the 9 times table, starting with 0
+ *
+ * Return: 0
+ */
+void times_table(void)
+{
+	int x;
+	int y;
+	int sum; 
+
+	for (x = 0; x < 10; x++)
+        {
+                for (y = 0; y < 10; y++)
+		{
+			sum = x * y;
+			_putchar('0' + sum);
+			if (x != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
