9a5c02857d59b21555b743ee7a3bea77be38d3b8
meddizzle316 <meddizzle316@gmail.com>
2023-09-22 09:23:51 -0700

    first attempt

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - printing the alphabet
+ *
+ * Return: the lowercase alphabet
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	i = 97;
+	for ( ; i < 123; )
+	{
+		_putchar(i)
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 
 int _putchar(char x);
+void print_alphabet(void);
 
 #endif
