a793cdfa301aaa3911c3cfb1edbd5cf1a35a98f6
meddizzle316 <meddizzle316@gmail.com>
2023-09-22 10:51:44 -0700

    fourth attempt bad feelings no no no

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -13,11 +13,11 @@ int print_sign(int n)
 	}
 	else if (n < 0)
 	{
-		return (-1);
 		_putchar('-');
+		return (-1);
 	}
 	else
-	{	return (0);
-		_putchar(0);
+	{	_putchar(0);
+		return (0);
 	}
 }
