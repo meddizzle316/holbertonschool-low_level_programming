b11e41902db18abfac9b61e5a7a3c465a8244318
meddizzle316 <meddizzle316@gmail.com>
2023-09-22 11:20:37 -0700

    first attempt

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -6,16 +6,16 @@
  */
 int _abs(int n)
 {
-	if (n > 0)
+	if (n < 0)
 	{
-		return (n);
+		return (n * -1);
 	}
 	else if (n == 0)
 	{
 		return (n);
 	}
-	else (n < 0)
+	else
 	{
-		return (n * -1);
+		return (n);
 	}
 }
