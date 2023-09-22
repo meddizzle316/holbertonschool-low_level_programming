3e7df2d7f3b4cad548cab9bab6f83ce9c99d1642
meddizzle316 <meddizzle316@gmail.com>
2023-09-22 11:51:12 -0700

    trying stuff

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -9,9 +9,10 @@ int print_last_digit(int n)
 	int last_d;
 
 	last_d = n % 10;
-	if (last_d > 47 && last_d < 58)
+	if (last_d > 47)
 	{
-		_putchar('0' + last_d);
-		return (last_d);
+		last_d *= -1;
 	}
+	_putchar('0' + last_d);
+	return (last_d);
 }
