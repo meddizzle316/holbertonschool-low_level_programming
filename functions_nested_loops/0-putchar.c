25ee8e48774b04c4f27da31c32ae22ab82d9a6b2
meddizzle316 <meddizzle316@gmail.com>
2023-09-22 08:25:43 -0700

    git wrecked

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -7,14 +7,6 @@
  */
 int _putchar()
 {
-	putchar("_");
-	putchar("p");
-	putchar("u");
-	putchar("t");
-	putchar("c");
-	putchar("h");
-	putchar("a");
-	putchar("r");
-	putchar("\n");
+	printf("_putchar");
 	return (0);
 }
