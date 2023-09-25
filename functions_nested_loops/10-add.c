21604111b860fbbb82ed036b3041201d868df368
meddizzle316 <meddizzle316@gmail.com>
2023-09-25 07:26:45 -0700

    trying task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adding two integers
+ * @x: first integer
+ * @y: second integer
+ *
+ * Return: the result
+ */
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}
