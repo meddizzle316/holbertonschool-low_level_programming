20ad34220216aeb14e4bcf49fc04d003d32cd632
meddizzle316 <meddizzle316@gmail.com>
2023-09-27 13:09:29 -0700

    just pushing something

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * print_triangle - printing a triangle followed by new line
+ * @size: size of triangle
+ * Return: a triangle
+ */
+void print_triangle(int size)
+{
+	if (size > 0)
+	{
+		int height;
+		int width;
+		int spaces;
+
+		if (size > 0)
+		{
+			for (height = 0; height < size; height++)
+			{
+				for (width = 0; width < size; width++)
+				{
+					for (spaces = size; spaces > 0; spaces--)
+					{
+						if (width <= spaces)
+						{
+							_putchar(35);
+						}
+						else 
+						{
+							_putchar(' ');
+						}
+					}
+				}
+				_putchar('\n');
+			}
+		}
+	else 
+	{
+		_putchar('\n');
+	}
+}
