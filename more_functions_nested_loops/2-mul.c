19a4dbad65517c2e91f20de0cdc59662977e7813
meddizzle316 <meddizzle316@gmail.com>
2023-09-27 07:56:53 -0700

    not sure about the return value

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: 1
+ */
+int mul(int a, int b)
+{
+	if (a > 47 && a < 58 && b > 47 && b < 58)
+	{
+		a * b; 
+	}
+}
