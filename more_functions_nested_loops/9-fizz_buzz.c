3ad362a46a0491caf2a8bd19e59ae557cf0b844e
meddizzle316 <meddizzle316@gmail.com>
2023-09-27 11:27:04 -0700

    cant copy so just trying it

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 101; i++)
+	for (i = 1; i < 101; i++)
 	{
 		if ((i % 3) == 0)
 		{
@@ -27,6 +27,7 @@ int main(void)
 		{
 			printf("%d ", i);
 		}
+		printf('\n');
 	}
 	return (0);
 }
