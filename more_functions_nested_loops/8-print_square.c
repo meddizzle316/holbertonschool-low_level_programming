2249bac19983abbee2276c27b91a2f83c69106b8
meddizzle316 <meddizzle316@gmail.com>
2023-09-27 11:07:31 -0700

    I GOT A FEELING OOO OOO

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - prints a square followed by new line
+ * @size: size of square
+ * Return: printing a square
+ */
+void print_square(int size)
+{
+	int height;
+	int width;
+
+	if (size > 0)
+	{
+		for (height = 0; height <= size; height++)
+		{
+			for (width = 0; width <= size; width++)
+			{
+				_putchar(35);
+			}
+		}
+	}
+	else 
+	{
+		_putchar('\n');
+	}
