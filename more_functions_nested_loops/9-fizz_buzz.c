6a6d59a46ba9ea6f48e7b94665913f0527cd7fe6
meddizzle316 <meddizzle316@gmail.com>
2023-09-27 11:22:47 -0700

    cant copy so just trying it

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 101; i++)
+	{
+		if ((i % 3) == 0)
+		{
+			printf("Fizz");
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		else if ((i % 5) == 0 && (i % 3) == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else 
+		{
+			printf(i);
+		}
+	}
+	return (0);
+}
