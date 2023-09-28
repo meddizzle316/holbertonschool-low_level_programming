e065d343d6fabf03fd3059d95e2ffa53e6e4ae4f
meddizzle316 <meddizzle316@gmail.com>
2023-09-28 08:52:28 -0700

    giving this a shot

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse, followed by newline
+ * @s: given string to reverse
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int i; 
+	int x;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+	
+	}
+	for (x = i; x > 0; x--)
+	{
+		_putchar(s[x]);
+	}
+}
