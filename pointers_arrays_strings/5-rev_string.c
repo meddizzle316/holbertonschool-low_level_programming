d525fc531189e232e9c1ad4a70cc5698ffcdc966
meddizzle316 <meddizzle316@gmail.com>
2023-09-28 11:19:55 -0700

    bleeh

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: given string
+ * Return: return value
+ */
+void print_rev(char *s)
+{
+        char tmp;
+        int i, len, len1;
+        len = 0;
+        len1 = 0;
+        while (s[len] != '\0')
+        {
+                len++;
+        }
+        len1 = len - 1;
+        for (i = 0; i < len / 2; i++)
+        {
+                tmp = s[i];
+                s[i] = s[len1];
+                s[len1--] = tmp;
+        }
+}
