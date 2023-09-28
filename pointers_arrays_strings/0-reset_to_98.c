bff4f769c77cd2889d9d1a8723c11bc2ce90d718
meddizzle316 <meddizzle316@gmail.com>
2023-09-28 07:46:18 -0700

    trying out pointer

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer as an int and updates its value to 98
+ * @n: the first int parameter
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	int* ptr = &n;
+	*ptr = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+a project about pointers in week 4 of T1 of Holberton Tulsa School
