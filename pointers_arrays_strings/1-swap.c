d0338a67f9c60aca41523289a61843d909975ad6
meddizzle316 <meddizzle316@gmail.com>
2023-09-28 07:58:40 -0700

    just giving this a shot

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swaps values of two integers
+ * @*a: first int pointer
+ * @*b: second int pointer
+ * Return: return value
+ */
+void swap_int(int *a, int *b)
+{
+	int *ptra = a; 
+	int *ptrb = b;
+
+	*ptra = b;
+	*ptrb = a;
+}
