e18cd32f8c95569ca7497a7cc0891b928cad3a62
meddizzle316 <meddizzle316@gmail.com>
2023-09-29 14:54:29 -0500

    giving this a shot

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -7,15 +7,16 @@
 int _atoi(char *s)
 {
 	int res = 0;
-	int i;
-	int isNegative;
+	int i, isNegative, hasNumber;
 
+	hasNumber = 0; 
 	isNegative = 0;
 	for (i = 0; s[i] != '\0'; ++i)
 	{
 		if (s[i] >= 48 && s[i] <= 57)
 		{
 		      res = res*10 + s[i] - '0';
+		      hasNumber = 1;
 		}
 		if (s[i] == '-' && isNegative == 0)
 		{
@@ -24,6 +25,10 @@ int _atoi(char *s)
 		else if (s[i] == '-' && isNegative == 1)
 		{
 			isNegative = 0; 
+		}
+		else if (s[i] == ' ' && hasNumber == 1)
+		{
+			break;
 		}
 	}  
 	if (isNegative == 1)
