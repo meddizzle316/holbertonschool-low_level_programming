b7cb5a9d2e2414e74a7097cf05049d8611065983
meddizzle316 <meddizzle316@gmail.com>
2023-09-29 13:48:16 -0500

    giving this a shot

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -6,26 +6,13 @@
  */
 int _atoi(char *s)
 {
-	int i;
-	int result;
-	int hasInteger;
-
-	hasInteger = 0;
-	for (i = 0; s[i] != '\0'; i++)
+	int res = 0;
+	for (int i = 0; s[i] != '\0'; ++i)
 	{
-		if (s[i] >= 0 && s[i] <= 9)
+		if (s[i] >= 48 && s[i] <= 57)
 		{
-			_putchar(s[i]);
-			hasInteger = 1;
-		        result += s[i];	
+		      res = res*10 + s[i] - '0';
 		}
-	}
-	if (hasInteger == 1)
-	{
-		return (result);
-	}
-	else
-	{
-		return (0);
-	}
+	}  
+	return res; 
 }
