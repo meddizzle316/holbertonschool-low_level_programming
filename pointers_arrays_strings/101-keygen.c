a30f6ae1c12529b0fb2e78059403961e16e73574
meddizzle316 <meddizzle316@gmail.com>
2023-10-02 11:37:52 -0500

    my commit message

---
+++ pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * function name - function description
+ * @parameters: description
+ * Return: return value
+ */
+int main (void)
+{
+	int i;
+
+	for (i = 0; i < 15; i++)
+	{
+		printf("%d\n", rand());
+	}
+	return (0);
+}
