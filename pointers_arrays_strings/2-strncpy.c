e19cda0b855a017e2fa3e36462eb7e562a0ca442
meddizzle316 <meddizzle316@gmail.com>
2023-10-04 11:18:57 -0500

    first commit

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,11 +11,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 	
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && i < src[i]; i++)
 	{
 		dest[i] = src[i];
 	}
-	while (n < i)
+	while (i < n)
 	{
 		dest[i] = '\0';
 		i++;
