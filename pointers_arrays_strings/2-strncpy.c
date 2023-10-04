792e794bf814f183ae97204a73bc0925bfb68ec5
meddizzle316 <meddizzle316@gmail.com>
2023-10-04 10:51:46 -0500

    first commit

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * _strncpy- copies a given string
+ * @dest: output -- the copied string
+ * @src: input -- string to be copied
+ * @n: number of bytes/characters from src to be copied to dest?
+ * Return: return value
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
