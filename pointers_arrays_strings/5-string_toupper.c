f60dc1aa247ed272addb9b715229eef44df05244
meddizzle316 <meddizzle316@gmail.com>
2023-10-04 13:37:36 -0500

    giving this a shot

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @src: a given string (do I need to give it a name?)
+ * Return: returns a new string with all uppercase
+ */
+char *string_toupper(char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+	{
+		if (src[i] >= 97 && src[i] <= 122)
+		{
+			src[i] -= 32;  
+		}
+	}
+	return (src);	
+}
