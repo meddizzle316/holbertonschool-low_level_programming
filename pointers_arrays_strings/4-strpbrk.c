bd0030b425abb1f9b5a475accae608a1ab4b9f73
meddizzle316 <meddizzle316@gmail.com>
2023-10-05 14:29:01 -0500

    giving this a shot

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * function name - function description
  * @parameters: description
@@ -6,5 +7,19 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	
+	int i, x;
+	char *j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (x = 0; accept[x]; x++)
+		{
+			if (s[i] == accept[x])
+			{
+				j = &s[i];
+				return (j);
+			}
+		}
+	}
+	return (NULL);
 }
