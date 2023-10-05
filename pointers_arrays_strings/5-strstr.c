f810a3af2029a1f3d04a45c7a3d472173fb24175
meddizzle316 <meddizzle316@gmail.com>
2023-10-05 15:00:41 -0500

    first commit

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * function name - function description
+ * @parameters: description
+ * Return: return value
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, x;
+
+	for (i = 0; haystack[i]; i++)
+	{
+		for (x = 0; needle[x]; x++)
+		{
+			if (haystack[i] == needle[x] && haystack[x])
+			{
+				if (haystack[i + 1] == needle[x + 1])
+				{
+					if (haystack[i + 2] == needle[x + 2])
+					{
+						return (char *) haystack +  i;
+					}
+				}
+			}
+		}
+	}
+	return (NULL);
+}
