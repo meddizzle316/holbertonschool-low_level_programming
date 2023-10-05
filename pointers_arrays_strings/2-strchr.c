33371846cad1925ecd254793644e02f42e382c8c
meddizzle316 <meddizzle316@gmail.com>
2023-10-05 11:23:41 -0500

    just committing

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -12,10 +12,7 @@ char *_strchr(char *s, char c)
 
 	for (i = 0; s[i]; i++)
 	{
-		if (s[i] == c)
-		{
-			return (char *) s + i;
-		}
+		if (s[i] == c) return (char *) s + i;
 	}
 	return (NULL);
 }
