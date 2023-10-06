af286eeed4367f3022ee06de7ed5771cc7057cde
meddizzle316 <meddizzle316@gmail.com>
2023-10-06 12:14:57 -0500

    hope this works

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * function name - function description
  * @parameters: description
@@ -7,16 +6,30 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int result, i, x;
+        int leftresult, rightresult, i, x, row, col;
 
-	result = x = 0;
-	for (i = 0; i < size * size; i++)
-	{
+        leftresult = rightresult = x = 0;
+        for (i = 0; i < size * size; i++)
+        {
+          row = i / (size + 1);
+          col = i % (size);
 
-			if (a[i] == a[x])
-			{
-				result += a[i];
-				x++;
-			}
-	}
+              if (row == x)
+                {
+                  leftresult += a[i];
+                  x++;
+                }
+        }
+        x = size - 1;
+        for (i = 0; i < size * size; i++)
+        {
+          row = i / (size + 1);
+          col = i % (size);
+
+              if (col == x)
+                {
+                  rightresult += a[i];
+                  x--;
+                }
+        }
 }
