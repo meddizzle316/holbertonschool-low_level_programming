38532e0dbfdc01daf4e8d3e437a4dce64fb571e6
meddizzle316 <meddizzle316@gmail.com>
2023-10-08 15:29:30 -0500

    hopefully this is better code

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -3,10 +3,10 @@
 #include "2-strlen.c"
 /**
  * _strstr - locates a substring
- * @haystack: a string 
+ * @haystack: a string
  * @needle: the substring we're checking the string to see if it's in there
  *
- * Return: the pointer to the location the string is 
+ * Return: the pointer to the location the string is
  */
 char *_strstr(char *haystack, char *needle)
 {
@@ -17,32 +17,24 @@ char *_strstr(char *haystack, char *needle)
 	lenofhaystack = _strlen(haystack);
 	for (i = 0; i <= lenofhaystack; i++)
 	{
-		for (x = 0; x <= lenofneedle; x++)
+		if (haystack[i] != needle[x])
 		{
-
-			if (haystack[i] == needle[x])
+			break;
+		}
+		while (haystack[i] == needle[x])
+		{
+			storei = i;
+			storex = x;
+			if (haystack [i - 1] == needle[x - 1] && x == lenofneedle - 1)
 			{
-				storei = i;
-				storex = x;
-				while (haystack[i] == needle[x])
-				{
-					if (haystack[i - 1] == needle[x - 1] && x == lenofneedle - 1)
-					{
-						return (char *) (haystack + storei);
-					}
-					i++;
-					x++;
-				}
-				i = storei;
-				x = storex;
+				return (char *) (haystack + storei);
 			}
-			if (needle[x] == '\0' && haystack[i] == '\0')
-			{
-				if (lenofneedle == 0)
-				{
-					return (char *) (haystack);
-				}
-			}
+			i = storei;
+			x = storex;
+		}
+		if (lenofneedle == 0)
+		{
+			return (char *) (haystack);
 		}
 	}
 	return (NULL);
