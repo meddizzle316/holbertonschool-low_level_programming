979b8f1b39bdf1527c4454800b9adc53b1762755
meddizzle316 <meddizzle316@gmail.com>
2023-10-07 23:33:40 -0500

    pleasing betty hopefully

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,37 +1,38 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * function name - function description
- * @parameters: description
- * Return: return value
+ * print_diagsu -- prints the diagonals in an array of ints
+ * @a: pointer to arrays of ints
+ * @size: size of arrays
+ *
+ * Return: sum of diagonals
  */
 void print_diagsums(int *a, int size)
 {
-        int leftresult, rightresult, i, x, row, col;
+	int leftresult, rightresult, i, x, row, col;
 
-        leftresult = rightresult = x = 0;
-        for (i = 0; i < size * size; i++)
-        {
-          row = i / (size + 1);
-          col = i % (size);
-
-              if (row == x)
-                {
-                  leftresult += a[i];
-                  x++;
-                }
-        }
-        x = size - 1;
-        for (i = 0; i < size * size; i++)
-        {
-          row = i / (size + 1);
-          col = i % (size);
-
-              if (col == x)
-                {
-                  rightresult += a[i];
-                  x--;
-                }
-        }
+	leftresult = rightresult = x = 0;
+	for (i = 0; i < size * size; i++)
+	{
+		row = i / (size + 1);
+		col = i % (size);
+		if (row == x)
+		{
+			leftresult += a[i];
+			x++;
+		}
+	}
+	x = size - 1;
+	for (i = 0; i < size * size; i++)
+	{
+		row = i / (size + 1);
+		col = i % (size);
+		
+		if (col == x)
+		{
+			rightresult += a[i];
+			x--;
+		}
+	}
 	printf("%d, %d\n", leftresult, rightresult);
 }
