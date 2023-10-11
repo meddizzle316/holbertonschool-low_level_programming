25dbea7ef2a82b1f96c38e338aa8ada91854791d
meddizzle316 <meddizzle316@gmail.com>
2023-10-11 13:48:18 -0500

    first try

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,14 +1,18 @@
 #include "main.h"
 /**
- * factorial - returns the factorial of a given number
+ * factoria? - returns the factorial of a given number
  * @n: given number
  * Return: the factorial
  */
 int factorial(int n)
 {
-	if (n <= 1)
+	if (n == 1)
 	{
 		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
 	}
 	else
 	{
