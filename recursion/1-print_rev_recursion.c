0be62fd29280f2a5075947bfc75714f0700afabf
meddizzle316 <meddizzle316@gmail.com>
2023-10-11 11:00:19 -0500

    giving this a shot

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,6 +1,5 @@
 #include "main.h"
-#include "_strlen.c"
-#include "0-puts_recursion.c"
+
 /**
  * _print_rev_recursio? - a function that prints a string in reverse
  * @s: the given string
@@ -8,19 +7,10 @@
  */
 void _print_rev_recursion(char *s)
 {
-	char *temp;
 
-	temp = s;
-	if (*temp != '\0')
+	if (*s)
 	{
-		_puts_recursion(temp);
-	}
-	else if (*temp == '\0')
-	{
-		s = temp;
-		s--;
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
-		_print_rev_recursion(s);
 	}
-
 }
