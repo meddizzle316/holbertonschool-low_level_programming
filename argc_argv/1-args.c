db08d529d054446378a53470ddd27f2939bf4939
meddizzle316 <meddizzle316@gmail.com>
2023-10-12 18:12:02 -0500

    seeing if this works

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the number of arguments passed to it
+ * @argc: number of arguments
+ * @argv: the array in which arguments are kept, of size argc
+ * Return: number of arguments
+ */
+int main(int argc, char **a_argv)
+{
+	printf("%d\n", argc);
+}
