4f219e1b1f6fcfa4cbb73b5bd19147299b75d81e
meddizzle316 <meddizzle316@gmail.com>
2023-10-12 10:42:07 -0500

    first try heres to hoping

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _sqrt_recursio? - finds the natural square root of n, else -1
+ * @n: given number
+ * Return: the square root or -1
+ */
+int helper(int n, int m)
+{
+	if (n == 1)
+	{
+		return (n);
+	}
+	else if (m <= 1)
+	{
+		return (-1);
+	}
+	else if (n / m == m && n % m == 0)
+	{
+		return (m);
+	}
+	else
+	{
+		return (helper(n, m - 1));
+	}
+}
+
+int _sqrt_recursion(int n)
+{
+	int m = helper(n, n);
+	return (m);
+}
