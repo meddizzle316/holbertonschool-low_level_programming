2ee7fa5374819701cdb8dd163bb6417dddb213ab
meddizzle316 <meddizzle316@gmail.com>
2023-10-12 20:14:09 -0500

    still wont work

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -3,14 +3,14 @@
 #include <stddef.h>
 #include <stdlib.h>
 /**
- * main - multiplies two numbers
+ * mai? - multiplies two numbers
  * @argc: number of arguments including program name
  * @argv: array in which arguments are held of size argc
  * Return: result of two multiplied numbers or Error and -1 if no numbers
  */
 int main(int argc, char *argv[])
 {
-	if (argv[argc - 2] == NULL || argv[argc - 1] == NULL)
+	if (argv[1] == NULL || argv[2] == NULL)
 	{
 		printf("Error\n -1");
 	}
@@ -18,8 +18,8 @@ int main(int argc, char *argv[])
 	{
 		int res, num1, num2;
 
-		num1 = strtol(argv[1],NULL,10);
-		num2 = strtol(argv[2],NULL,10);
+		num1 = strtol(argv[argc - 2],NULL,10);
+		num2 = strtol(argv[argc - 1],NULL,10);
 		res = num1 * num2;
 		printf("%d\n", res);
 	}
