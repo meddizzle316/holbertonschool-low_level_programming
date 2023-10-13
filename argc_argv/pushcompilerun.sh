3a03a3b89cbf8c536cebc1a9b9fa21acbcd57a1c
meddizzle316 <meddizzle316@gmail.com>
2023-10-13 10:33:16 -0500

    trying out this

--- argc_argv/pushcompilerun.sh
+++ argc_argv/pushcompilerun.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 "$1" -o "${1%.c}.o"
-./"${1%.c}.o"
-mv "${1%.c}.o" backups/
+input="$1"
+output="${1%.c}.o"
+destination="backups/"
+comment="$2"
+
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 "$input" -o "$output"
+./"$output"
+mv "$output" "$destination"
 git add .
-git commit -m "$2"
+git commit -m "$comment"
 git push
