9195be63663a5e2b07d01ee4fe52e508a42361b7
meddizzle316 <meddizzle316@gmail.com>
2023-10-12 20:06:13 -0500

    still wont work

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 
 		num1 = strtol(argv[1],NULL,10);
 		num2 = strtol(argv[2],NULL,10);
-		res = num1 + num2;
+		res = num1 * num2;
 		printf("%d\n", res);
 	}
 	return (0);
