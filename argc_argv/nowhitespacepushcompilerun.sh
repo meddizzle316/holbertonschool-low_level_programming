f29e2d8750bed41d618a36dd35e6f0a8d69c1a6f
meddizzle316 <meddizzle316@gmail.com>
2023-10-13 10:20:19 -0500

    trying this out

--- argc_argv/nowhitespacepushcompilerun.sh
+++ argc_argv/nowhitespacepushcompilerun.sh
@@ -2,6 +2,7 @@
 sed --in-place 's/[[:space:]]\+$//' "$1"
 gcc -Wall -pedantic -Werror -Wextra -std=gnu89 "$1" -o "${1%.c}.o"
 ./"${1%.c}.o"
+mv "${1%.c}.o" backups/
 git add .
 git commit -m "$2"
 git push
