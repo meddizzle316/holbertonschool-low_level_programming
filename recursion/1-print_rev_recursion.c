5df1d836472793a8094883029b1682683364661f
meddizzle316 <meddizzle316@gmail.com>
2023-10-15 13:09:10 -0500

    final push

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_rev_recursio? - a function that prints a string in reverse
+ * _print_rev_recursion - a function that prints a string in reverse
  * @s: the given string
  * Return: NOTHING it's void
  */
