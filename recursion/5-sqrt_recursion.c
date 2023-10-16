54a2eaada518cd8230328840a0b10f705dc71744
meddizzle316 <meddizzle316@gmail.com>
2023-10-16 12:10:20 -0500

    trying this out

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -6,34 +6,32 @@
  */
 float helper(int n, float m, float temp)
 {
-        if (m == temp)
-        {
-                return (m);
-        }
-        else
-        {
-                temp = m;
-                m = (n/temp + temp) / 2;
-                return helper(n, m, temp);
-        }
-
+	if (m == temp)
+	{
+		return (m);
+	}
+	else
+	{
+		temp = m;
+		m = (n/temp + temp) / 2;
+		return helper(n, m, temp);
+	}
 }
 
 int _sqrt_recursion(int n)
 {
-        float temp = 0;
-        if (n < 1)
-        {
-          return (-1);
-        }
-        if (n == 1)
-        {
-          return (n);
-        }
-        else
-        {
-          float m = helper(n, (n/2), temp);
-          return (m);
-
+	float temp = 0;
+	if (n < 1)
+	{
+		return (-1);
+	}
+	if (n == 1)
+	{
+		return (n);
+	}
+	else
+	{
+		float m = helper(n, (n/2), temp);
+		return (m);
         }
 }
