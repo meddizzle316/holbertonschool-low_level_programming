e6a7e4c317ec50b330ef3f9810c96e97236b8ea2
meddizzle316 <meddizzle316@gmail.com>
2023-10-18 11:20:27 -0500

    trying stuff out

--- malloc_free/5-strlen.c
+++ malloc_free/5-strlen.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include "main.h"
 /**
  * _strlen - returns the length of a string
@@ -7,7 +8,11 @@
 int _strlen(char *s)
 {
 	int length;
-
+	
+	if (s == NULL)
+	{
+		return (0);
+	}
 	for (length = 0; s[length] != 0; length++)
 	{
 	}
