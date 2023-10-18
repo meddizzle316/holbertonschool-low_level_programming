1b00f9d776d2e5d25929130fe51bf92db30ef1c8
meddizzle316 <meddizzle316@gmail.com>
2023-10-18 10:17:13 -0500

    trying this out

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stddef.h>
 /**
- * *create_array - creates an array of chars and initializes it with a specific char
+ * *create_array - creates an array of chars and initializes it with a char
  * @size: size of array
  * @c: char to fill entire array
  * Return: array of char with c of size size
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	char *new_array;
 	unsigned int i;
 
-	new_array = malloc(sizeof(char) * (size + 1));
+	new_array = malloc(sizeof(char) * (size));
 
 	if (size <= 0)
 	{
