8abf88932a1e781ebfc09233dac4a460c14fb858
meddizzle316 <meddizzle316@gmail.com>
2023-10-18 10:57:12 -0500

    trying stuff out

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -2,6 +2,7 @@
 #include "5-strlen.c"
 #include <stdlib.h>
 #include <stdio.h>
+#include <stddef.h>
 /**
  * _strdup - returns a pointer to a newly allocated copied string given
  * @str: string given
@@ -14,7 +15,12 @@ char *_strdup(char *str)
 	int len = _strlen(str);
 
 	str = malloc(sizeof(char) * (len + 1));
-	new_string = malloc (sizeof(char) * (len + 1));
+	new_string = malloc(sizeof(char) * (len + 1));
+
+	if (str == NULL || new_string == NULL || len <= 0)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < len; i++)
 	{
 		new_string[i] = str[i];
