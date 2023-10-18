0e43d3e50b10c6efa807257eca9eb9f2b4032eb5
meddizzle316 <meddizzle316@gmail.com>
2023-10-18 11:41:21 -0500

    trying stuff

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -15,7 +15,7 @@ char *_strdup(char *str)
 	int len = _strlen(str);
 
 	new_string = malloc(sizeof(char) * (len + 1));
-
+	
 	if (str == NULL || new_string == NULL || len <= 0)
 	{
 		return ('\0');
---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include "5-strlen.c"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * str_concat - function description
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to a new string with both
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int len1, len2, totallen, i, x;
+	char *concat_string;
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	totallen = len1 + len2;
+
+	concat_string = malloc(sizeof(char) * (totallen));
+
+	for (i = 0; s1[i]; i++)
+	{
+		concat_string[i] = s1[i];
+	}
+	for (x = 0; s2[x]; x++, i++)
+	{
+		concat_string[i] = s1[x];
+	}
+	return (concat_string);
+
+}
