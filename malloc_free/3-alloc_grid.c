dfbe0731d412555a886f638049da3210151d5f61
meddizzle316 <meddizzle316@gmail.com>
2023-10-18 12:31:07 -0500

    first shot into the dark

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,4 +1,7 @@
 #include "main.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
 /**
  * alloc_grid - returns a pointer to a 2d array of int
  * @width: width of array
@@ -7,5 +10,22 @@
  */
 int **alloc_grid(int width, int height)
 {
-	
+	int **new_grid;
+	int i, x;
+
+	new_grid = malloc(sizeof(int) * width);
+
+	if (new_grid == NULL || width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		new_grid[i] = malloc(sizeof(int) * width);
+		for (x = 0; x < width; x++)
+		{
+			new_grid[i][x] = 0;
+		}
+	}
+	return (new_grid);
 }
