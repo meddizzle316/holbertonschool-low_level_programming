7f9c5157f69bc2c249667b79e50bf44a8ae61076
meddizzle316 <meddizzle316@gmail.com>
2023-10-20 14:06:49 -0500

    probably wont work

---
+++ preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: name of file it was compiled from
+ */
+int main ()
+{
+	char file_name[] = "2-main.c";
+	printf("%s\n",file_name);
+	return (0);
+}
