e066e3479e30c7ab0c3aef8c44549c0c0b96868a
meddizzle316 <meddizzle316@gmail.com>
2023-10-20 11:05:22 -0500

    trying this out

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
-	char *new_array;
+	int *new_array;
 
 	new_array = malloc((size * nmemb));
 	if (new_array == NULL || nmemb <= 0 || size <= 0)
