62087cc57c3c2bd1b5b46bc1f41b2e05dc991c44
meddizzle316 <meddizzle316@gmail.com>
2023-10-20 10:35:30 -0500

    trying this out

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -19,10 +19,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		free(new_array);
 		return (NULL);
 	}
-	for (i = 0; new_array[i]; i++)
+	for (i = 0; i < nmemb; i++)
 	{
 		new_array[i] =  0;
 	}
-	new_array[i] = '\0';
 	return (new_array);
 }
