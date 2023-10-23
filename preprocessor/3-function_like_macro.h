c5f76bc6bf8808c5ccebcb576b7bc6f3ec74a107
meddizzle316 <meddizzle316@gmail.com>
2023-10-23 15:33:35 -0700

    going for gold

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
-#define ABS(x - y)  ((x - y) > 0 ? x : x * -1)
+#define ABS(x)  ((x - y) > 0 ? x : x * -1)
 
 typedef int make_iso_compilers_happy;
 
