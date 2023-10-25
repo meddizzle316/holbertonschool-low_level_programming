3d25ba0aa126882f46900cc9b6712065c8fbb16d
meddizzle316 <meddizzle316@gmail.com>
2023-10-25 14:23:27 -0500

    trying this tout

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -1,14 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-struct dog
-{
-	char *name;
-	int age;
-	char *owner;
-};
 int _putchar(char c);
-void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 #endif
