21ca3adec5a71d342bc27364861e9d9c93b3c877
meddizzle316 <meddizzle316@gmail.com>
2023-10-25 14:19:12 -0500

    trying this tout

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -13,7 +13,9 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 
-	d.name -> name;
-	d.age -> age;
-	d.owner -> owner;
+	struct dog *d;
+
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
 }
