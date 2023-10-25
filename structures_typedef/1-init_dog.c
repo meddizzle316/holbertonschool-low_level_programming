07bd5c44e780f3365ee74937b13a89d72c6ef734
meddizzle316 <meddizzle316@gmail.com>
2023-10-25 14:28:00 -0500

    trying this tout

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
+
 /**
  * init_dog - initializes a struct of dog type
  * @d: a struct named d
@@ -14,6 +16,7 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 
+	d = (struct dog*) malloc (sizeof(struct dog));
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
