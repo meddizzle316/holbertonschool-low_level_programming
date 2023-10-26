83ff0bd15fdd9dbc999d529987abd4e5c15e538c
meddizzle316 <meddizzle316@gmail.com>
2023-10-26 11:45:18 -0500

    hope this works

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -22,7 +22,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog = (dog_t *)malloc(sizeof(dog_t));
 	new_dog->name = (char *)malloc(sizeof(char) * len1 + 1);
 	new_dog->owner= (char *)malloc(sizeof(char) * len2 + 1);
-	if (new_dog == NULL || new_dog->name == NULL || new_dog->owner == NULL || age == 0)
+	if (new_dog == NULL || new_dog->name == NULL || new_dog->owner == NULL || age < 0)
 	{
 		free(new_dog->owner);
 		free(new_dog->name);
@@ -30,7 +30,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	new_dog->name = name;
+	name = NULL;
 	new_dog->age = age;
 	new_dog->owner = owner;
+	owner = NULL;
 	return (new_dog);
 }
