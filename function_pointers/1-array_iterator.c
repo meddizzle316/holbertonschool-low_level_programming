52dc8f2d651d0c0de28aba1ac12e1e4410c0d89d
meddizzle316 <meddizzle316@gmail.com>
2023-10-27 14:06:46 -0500

    hope this works

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size && array != NULL && action != NULL && size > 0; i++)
 	{
 		action(array[i]);
 	}
