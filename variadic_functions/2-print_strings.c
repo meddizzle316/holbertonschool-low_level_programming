3850bd72c86010ff8cf59f742d3ce1bc5913becd
meddizzle316 <meddizzle316@gmail.com>
2023-10-30 13:20:04 -0500

    here goes nothing

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			}
 			else
 			{
-				printf("%s", "\0");
+				printf("%s", "(nil)");
 			}
 			if (separator != NULL)
 			{
