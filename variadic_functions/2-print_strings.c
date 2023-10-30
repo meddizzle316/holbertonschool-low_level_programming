885a47505d024dfa9b50a38ec659f9f84a2c535d
meddizzle316 <meddizzle316@gmail.com>
2023-10-30 13:00:59 -0500

    hope this works

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (i < n - 1)
 		{
 			printf("%s", va_arg(args, const char *));
-			printf("%s ", separator);
+			printf("%s", separator);
 		}
 		else if ( i == n - 1)
 		{
