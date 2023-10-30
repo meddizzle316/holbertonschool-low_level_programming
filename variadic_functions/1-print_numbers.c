8f9c9a4ee0e8e3bbd948be1db23b9c4a5c483747
meddizzle316 <meddizzle316@gmail.com>
2023-10-30 11:10:57 -0500

    here goes

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -17,9 +17,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		va_start(args, n);
 		for (i = 0; i < n; i++)
 		{
-			x = va_arg(args, int);
-			printf("%d, ", x);
-			if (i == n - 1)
+			if (i < n - 1)
+			{
+				x = va_arg(args, int);
+				printf("%d, ", x);
+			}
+			else if (i == n - 1)
 			{
 				printf("%d", x);	
 			}
