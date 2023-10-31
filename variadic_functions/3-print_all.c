f9473efe65024aabc1a302308c1408d187e01f95
meddizzle316 <meddizzle316@gmail.com>
2023-10-30 22:33:46 -0500

    dear lord let this work

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,17 +1,6 @@
 #include "variadic_functions.h"
-void print_char_string (char * placeholder)
-{
-	printf("%s", placeholder);
-}
-
-void print_int (int placeholder)
-{
-	printf("%d", placeholder);
-}
-
-
 /**
- * print_all - a function that prints anything
+ * print_al? - a function that prints anything
  * @format: a list of types of arguments passed to the function
  * @...: an ellipse that represents the variable arguments passed
  * Return: anything of c, i, f or char* type can be printed
@@ -21,22 +10,31 @@ void print_all(const char * const format, ...)
 	int i;
 	char *char_placeholder;
 	int int_placeholder;
+	double float_placeholder;
 	va_list(ap);
 
 	i = 0;
 	va_start(ap, format);
 	while (format[i] != '\0')
 	{
-		while (format[i] == 'c' || format[i] == 's')
-		{
-			char_placeholder = va_arg(ap, char *);
-			print_char_string(char_placeholder);
-		}
-		if (format[i] == 'i' || format[i] == 'f')
-		{
+		switch(format[i]) {
+		case 'c':
+			char_placeholder = va_arg(ap, char*);
+			printf("%s", char_placeholder);
+			break;
+		case 'i':
 			int_placeholder = va_arg(ap, int);
-			print_int(int_placeholder);
-		}	
+			printf("%d", int_placeholder);
+			break;
+		case 's':
+			char_placeholder = va_arg(ap, char *);
+			printf("%s", char_placeholder);
+			break;
+		case 'f':
+			float_placeholder = va_arg(ap, double);
+			printf("%f", float_placeholder);
+			break;
+		}
 		if (format[i + 1] != '\0' && ((format[i] == 's' 
 		|| format[i] == 'c' 
 		|| format[i] == 'i' 
