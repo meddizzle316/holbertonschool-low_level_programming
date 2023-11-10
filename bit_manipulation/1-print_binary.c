4131d49fbee2f2d649d035eed0da47d82321b750
meddizzle316 <meddizzle316@gmail.com>
2023-11-10 14:11:53 -0600

    first push of 1

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_binary - prints a binary without arrays, /, % or malloc
+ * @n: integer to be printed into binary
+ * Return: always void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int x;
+
+	x = 0;
+	if (n > 0)
+	{
+		x = n & 1;
+		n = n >> 1;
+		print_binary(n);
+	}
+	_putchar('0' + x);
+}
