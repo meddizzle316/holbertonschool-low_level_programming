36cce00e2b55c9048fbee15e193a811c2a18d14b
meddizzle316 <meddizzle316@gmail.com>
2023-11-10 14:46:22 -0600

    added case for too long index

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -6,5 +6,19 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	return ((n & (1 << index)) != 0);
+	unsigned long int x;
+	unsigned long int length;
+	
+	length = 0;
+	x = n;
+	while (x != 0)
+	{
+		x /= 10;
+		length++;
+	}
+	if (length > index)
+	{
+		return ((n & (1 << index)) != 0);
+	}
+	return (-1);
 }
