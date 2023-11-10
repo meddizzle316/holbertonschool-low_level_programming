141aa7f4f2ca6fd5b8a19e978ee6a8d54686094b
meddizzle316 <meddizzle316@gmail.com>
2023-11-10 10:23:55 -0600

    added free_null function to save space

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+void free_null(listint_t **ptr);
 /**
  * delete_nodeint_at_index - deletes int node at index
  * @head: head of list
@@ -15,6 +16,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (temp == NULL)
 	{
 		free(temp);
+		temp = NULL;
 		return (-1);
 	}
 	if (*head != NULL)
@@ -24,8 +26,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			if (index == 0)
 			{
 				*head = (*head)->next;
-				free(current);
-				free(temp);
+				free_null(&current);
+				free_null(&temp);
 				return (1);
 			}
 			else if (i == index - 1)
@@ -35,12 +37,23 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			else if (i == index)
 			{
 				temp->next = current->next;
-				free(current);
-				free(temp);
+				free_null(&current);
+				free_null(&temp);
 				return (1);
 			}
 		}
 	}
-	free(temp);
+	free_null(&temp);
 	return (-1);
+}
+
+/**
+ * free_null -- frees a node and sets it to zero; space saver
+ * @ptr: double pointer to listint_t to be freed
+ * Return: Always void
+ */
+void free_null(listint_t **ptr)
+{
+	free(*ptr);
+	*ptr = NULL;
 }
