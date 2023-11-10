838f8da8d4020259a46e3776f7aff357eeedbe83
meddizzle316 <meddizzle316@gmail.com>
2023-11-10 14:59:56 -0600

    just went with binary limits as limiting factor

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -5,18 +5,8 @@
  * Return: return value
  */
 int get_bit(unsigned long int n, unsigned int index)
-{
-	unsigned long int x;
-	unsigned long int length;
-	
-	length = 0;
-	x = n;
-	while (x != 0)
-	{
-		x /= 10;
-		length++;
-	}
-	if (length > index)
+{	
+	if (index < 32)
 	{
 		return ((n & (1 << index)) != 0);
 	}
