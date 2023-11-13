4bc0ddc1ece2fc2dafbe4e47d2f4a6a894c28caa
meddizzle316 <meddizzle316@gmail.com>
2023-11-13 17:31:15 -0600

    fixed O_CREAT

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 
 	i = fd = j = len = 0;
 
-	len = (strlen(text_content) + 1);
+	len = (strlen(text_content));
 	if (filename == NULL || text_content == NULL)
 	{
 		return (-1);
