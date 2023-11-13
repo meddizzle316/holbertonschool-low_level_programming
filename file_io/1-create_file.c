7e530d862968306fe20d8d1fd9515035dc570900
meddizzle316 <meddizzle316@gmail.com>
2023-11-13 17:25:29 -0600

    improving syntax

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (-1);
 	}
-	i = write(fd, text_content, sizeof text_content);
+	i = write(fd, text_content, sizeof(text_content));
 	if (i == -1)
 	{
 		close(fd);
