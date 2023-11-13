ab935c5e38147702af3246411697395421e08267
meddizzle316 <meddizzle316@gmail.com>
2023-11-13 17:57:40 -0600

    added permissions

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_CREAT, S_IRUSR | S_IWUSR | O_RDWR | O_RDONLY | O_TRUNC);
+	fd = open(filename, O_CREAT | O_RDWR | O_RDONLY | O_TRUNC);
 	if (fd == -1)
 	{
 		close(fd);
