b07821d8a9acffb4e599bfd404eaddb8463918a9
meddizzle316 <meddizzle316@gmail.com>
2023-11-13 17:29:37 -0600

    fixed O_CREAT

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * function name - function description
  * @parameters: description
@@ -6,10 +7,11 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int i, fd, j;
+	int i, fd, j, len;
 
-	i = fd = j = 0;
+	i = fd = j = len = 0;
 
+	len = (strlen(text_content) + 1);
 	if (filename == NULL || text_content == NULL)
 	{
 		return (-1);
@@ -20,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (-1);
 	}
-	i = write(fd, text_content, sizeof(text_content));
+	i = write(fd, text_content, len);
 	if (i == -1)
 	{
 		close(fd);
