2025d93fdd824994eb8e0f72d690b7912b7f6c36
meddizzle316 <meddizzle316@gmail.com>
2023-11-13 18:12:27 -0600

    created empty file if text_content is NULL

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -32,7 +32,7 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 		}
 		close(fd);
-		return (-1);
+		return (1);
 
 	}
 	else 
@@ -45,5 +45,4 @@ int create_file(const char *filename, char *text_content)
 		}
 		return (1);
 	}
-	return (1);
 }
