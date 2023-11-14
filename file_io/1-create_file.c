9caafd4f1bc0807955b29f3153ed21f2fad12199
meddizzle316 <meddizzle316@gmail.com>
2023-11-13 18:01:12 -0600

    fixed text_content null case

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,12 +12,11 @@ int create_file(const char *filename, char *text_content)
 	int i, fd, j, len;
 
 	i = fd = j = len = 0;
-
-	len = (strlen(text_content));
 	if (filename == NULL || text_content == NULL)
 	{
 		return (-1);
 	}
+	len = strlen(text_content);
 	fd = open(filename, O_CREAT | O_RDWR | O_RDONLY | O_TRUNC);
 	if (fd == -1)
 	{
