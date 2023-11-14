e2249e54868b837b19a4caf10ced3a73f1f2c8fa
meddizzle316 <meddizzle316@gmail.com>
2023-11-13 18:11:16 -0600

    created empty file if text_content is NULL

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,26 +9,41 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int i, fd, j, len;
+	int i, fd, len;
 
-	i = fd = j = len = 0;
-	if (filename == NULL || text_content == NULL)
+	i = fd = len = 0;
+	if (filename == NULL)
 	{
 		return (-1);
 	}
-	len = strlen(text_content);
-	fd = open(filename, O_CREAT | O_RDWR | O_RDONLY | O_TRUNC);
-	if (fd == -1)
+	if (text_content != NULL)
 	{
+		len = strlen(text_content);
+		fd = open(filename, O_CREAT | O_RDWR | O_RDONLY | O_TRUNC);
+		if (fd == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+		i = write(fd, text_content, len);
+		if (i == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 		close(fd);
 		return (-1);
+
 	}
-	i = write(fd, text_content, len);
-	if (i == -1)
+	else 
 	{
-		close(fd);
-		return (-1);
+		fd = open(filename, O_CREAT | O_RDWR | O_RDONLY | O_TRUNC);
+		if (fd == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+		return (1);
 	}
-	close(fd);
 	return (1);
 }
