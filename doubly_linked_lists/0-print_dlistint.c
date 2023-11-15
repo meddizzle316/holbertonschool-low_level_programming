f69b963f39baae7eb09f31af49483f67e3e76199
meddizzle316 <meddizzle316@gmail.com>
2023-11-15 10:04:15 -0600

    first attempt at 0

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints all elements of a dlistint_t
+ * @h: head of doubly linked list
+ * Return: amount of nodes in list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *current;
+	size_t i;
+
+	current = NULL;
+	i = 0;
+	if (h == NULL)
+	{
+		return (0);
+	}
+	current = h;
+	while (current != NULL)
+	{
+		printf("%d", current->n);
+		current = current->next;
+		i++;
+	}
+	return (i);
+}
