8b3d9cd9592908b9dc31bb913d2f85be00c466a3
meddizzle316 <meddizzle316@gmail.com>
2023-11-15 11:20:19 -0600

    added malloc return to 2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -2,6 +2,7 @@
 /**
  * add_dnodeint - adds a new node at beginning of dlistint_t
  * @head: double pointer to doubly linked list
+ * @n: data of new node
  * Return: pointer to new node
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
@@ -9,6 +10,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new;
 
 	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 	new->n = n;
 	new->prev = NULL;
 	new->next = NULL;
