fc28d630d62e77d9532a03654ad2aa38b5a67e0e
meddizzle316 <meddizzle316@gmail.com>
2023-11-15 10:05:50 -0600

    added newline

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -18,7 +18,7 @@ size_t print_dlistint(const dlistint_t *h)
 	current = h;
 	while (current != NULL)
 	{
-		printf("%d", current->n);
+		printf("%d\n", current->n);
 		current = current->next;
 		i++;
 	}
