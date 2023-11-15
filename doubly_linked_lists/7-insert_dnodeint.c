9c9fe8b0a80a5e55ad0b29fc7dfcdf6ff788d5f0
meddizzle316 <meddizzle316@gmail.com>
2023-11-15 13:46:58 -0600

    first go at 7

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given (idx) position
+ * @h: head of list
+ * @idx: index to insert node
+ * @n: content of a new node
+ * Return: pointer to new node or NULL if failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new;
+	dlistint_t *current;
+	unsigned int i;
+
+	i = 0;
+	current = NULL;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+	if (*h == NULL && idx == 0)
+	{
+		*h = new;
+		return (new);
+	}
+	current = *h;
+	while (current != NULL)
+	{
+		if (i == idx)
+		{
+			current->prev->next = new;
+			current->prev = new;
+			new->prev = current->prev;
+			new->next = current;
+			return(new);
+		}
+		i++;
+		current = current->next;
+	}
+	return (NULL);
+}
