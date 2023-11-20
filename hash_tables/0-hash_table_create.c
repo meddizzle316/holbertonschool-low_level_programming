3f6a30789cd6f59fe7455055b1361dc85d6692f5
meddizzle316 <meddizzle316@gmail.com>
2023-11-20 10:05:27 -0600

    betty fixes

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(ht);
 		return (NULL);
 	}
-	ht->array = malloc(sizeof(hash_node_t*) * size);
+	ht->array = malloc(sizeof(hash_node_t *) * size);
 	ht->size = size;
 	if (ht->array == NULL)
 	{
