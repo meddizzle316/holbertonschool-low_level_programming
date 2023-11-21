33b9a5882e625a6c5d453c2354ad1c3759dad7cc
meddizzle316 <meddizzle316@gmail.com>
2023-11-21 10:03:05 -0600

    betty fixes to 4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,20 +1,22 @@
 #include "hash_tables.h"
 /**
- * function name - function description
- * @parameters: description
- * Return: return value
+ * hash_table_get - gets a value at an index in a hash_table_t
+ * @ht: pointer to hash_table_t
+ * @key: pointer to key to compare
+ * Return: pointer to value at index of key or NULL if fails
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	int index;
 	hash_node_t *tmp;
-	
-	if (ht == NULL) return (NULL);
+
+	if (ht == NULL)
+		return (NULL);
 	index = key_index((unsigned const char *)key, ht->size);
 	tmp = ht->array[index];
 	while (tmp != NULL)
 	{
-		if (strcmp (tmp->key, key) == 0)
+		if (strcmp(tmp->key, key) == 0)
 		{
 			return (tmp->value);
 		}
