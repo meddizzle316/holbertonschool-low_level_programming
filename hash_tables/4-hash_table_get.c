01d5d449f3509f57c132ac758cd1d24522aabedc
meddizzle316 <meddizzle316@gmail.com>
2023-11-21 09:07:28 -0600

    null case for ht for 4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -8,7 +8,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	int index;
 	hash_node_t *tmp;
-
+	
+	if (ht == NULL) return (NULL);
 	index = key_index((unsigned const char *)key, ht->size);
 	tmp = ht->array[index];
 	while (tmp != NULL)
